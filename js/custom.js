76f82d6e0d4bfc8952a617dbea3b4a86b9dd940f
Jay Young <jayayoung@gmail.com>
2016-09-25 20:17:21 +1000

    Update start files

--- js/custom.js
+++ js/custom.js
@@ -1,13 +1,18 @@
+/*
+Creat a function to count up a tennis score. When one player wins, change the board title to display the winner.
+*/
 $(function(){
 
 var scorePlayer1 = 0;
 var scorePlayer2 = 0;
 
 
-$('#p1-point').click(player1game);
-$('#p2-point').click(player2game);
-
-
+$('#p1-point').click(function(){
+	player1game()
+});
+$('#p2-point').click(function(){
+	player2game();
+});
 
 
 
